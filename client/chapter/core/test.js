73c28bcdfaf167062edca676c1c5ea021f9823db
djswns7 <djswns7@naver.com>
2023-01-06 11:33:06 +0900

    [추가] Array 매소드

--- client/chapter/core/test.js
+++ client/chapter/core/test.js
@@ -1,49 +1,55 @@
 
-let title = "환영합니다.";
 
-var house = {
-  title: "집이예요~",
-  room: {
-    title: "방입니다.",
-    getTitle: function() {
-      return this.title;
+
+function first() {
+  let x = 10;
+
+  function second() {
+    let y = 30;
+    return x * y;
+  }
+
+  return second; // second()이렇게 바로 실행하면 값이 나오지만 의미가 없고?? 정해져서 나온 값이라서??
+}
+
+first // 설명서
+first() // 값 
+
+let value = first();
+
+value = function() {
+  let y = 30;
+  return 10 * y;
+}
+
+console.log(value());
+
+
+
+function a(x){
+  console.log(x);
+}
+
+
+class C{
+  variable1 = 'just example';
+  variable2 = 3;
+  variable3 = {
+    innerVar1 : 'just text',
+    innerVar2 : 33,
+  };
+}
+
+function F(){
+  return {
+    variable1 : 'just example',
+    variable2 : 3,
+    variable3 : {
+      innerVar1 : 'just text',
+      innerVar2 : 33,
     }
   }
 }
 
-console.log("------------var,let로 선언된 변수에 대한 this로 접근했을 때 다른관점 그리고 call함수------------------");
-var fn = house.room.getTitle;
-console.log("fn : " , fn);
-console.log("fn() : ", fn());
-console.log("fn.call(house) : ", fn.call(house));
-//집이에요~
-
-console.log("------------house객체 속 room객체 속 함수 사용------------------");
-console.log("house.room.getTitle() : ", house.room.getTitle());
-// 방입니다.
-
-console.log("------------room객체 속 함수를 house객체에 생성------------------");
-house.getTitle = house.room.getTitle;
-house.getTitle();
-console.log("house : " , house);
-console.log("house.getTitle : ", house.getTitle);
-console.log("house.getTitle() : ", house.getTitle());
-//집이예요~
-
-console.log("------------var,let로 선언된 변수에 대한 this로 접근했을 때 다른관점 그리고 call함수------------------");
-var fn2 = house.room.getTitle;
-console.log("fn2 : ", fn2);
-console.log("fn2() : ", fn2());
-console.log("fn2.call(house) : ", fn2.call(house));
-//집이에요~
-
-console.log("------------fn2를 풀어서 사용------------------");
-console.log("house.room.getTitle.call(house) : ", house.room.getTitle.call(house));
-//집이에요~
-
-console.log("------------------------------");
-house.getTitle = house.room.getTitle;
-house.getTitle.call(house);
-console.log("house.getTitle : ", house.getTitle);
-console.log("house.getTitle.call(house) : ", house.getTitle.call(house));
-//집이에요~
+let madeByClass = new C();
+let madeByFunction = F();
